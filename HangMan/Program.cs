47a1089fe9dab4eb080876514759b2cbc0b8e460
Axel <Axel@DESKTOP-OOMDLKG>
2022-03-28 17:44:14 +0200

    Add project files.

---
+++ HangMan/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HangMan
+{
+    class Program
+    {
+        static private protected Dictionary<string, int> randomWord = new Dictionary<string, int>()
+    {
+          { "shockingly", 1 },
+          { "humanizers", 2 },
+          { "hypocrites", 3 },
+          { "brutalized", 4 },
+          { "blueprints", 5 },
+          { "abductions", 6 },
+          { "nightmares", 7 },
+          { "hospitable", 8 },
+          { "destroying", 9 },
+          { "complaints", 10 }
+
+    };
+        public static char[] WordToChar(string word)
+        {
+            char[] charArr = word.ToCharArray();
+            foreach (char ch in charArr)
+            {
+
+                Console.WriteLine(ch);
+            }
+            return charArr;
+        }
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine(myValue);
+
+
+
+        }
+    }
+}
